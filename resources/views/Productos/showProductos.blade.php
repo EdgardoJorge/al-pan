58b7a1c7705f80fe659ea9f61581e4b631abd1aa
EdgardoJorge <i2310402@continental.edu.pe>
2024-05-18 08:56:56 -0500

    vista producto

--- resources/views/Productos/showProductos.blade.php
+++ resources/views/Productos/showProductos.blade.php
@@ -20,52 +20,63 @@
         </div>
     </div>
     <div class="col-start-3 col-end-13 mr-6 row-span-9 mb-6 rounded-xl bg-grayDebil grid grid-cols-10 grid-rows-8 gap-1">
-        <table class="w-full">
-        <thead>
-          <tr>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Valve</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Harina</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Produkt</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Lata</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Nombre</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Presentacion</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Sk</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Fecha de m</th>
-            <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left">Stack</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border py-4 px-4">a</td>
-            <td class="border py-4 px-4">Harina</td>
-            <td class="border py-4 px-4">Produkt</td>
-            <td class="border py-4 px-4">10023</td>
-            <td class="border py-4 px-4">Hanna sin preparar</td>
-            <td class="border py-4 px-4">10 kg</td>
-            <td class="border py-4 px-4">05-04-2004</td>
-            <td class="border py-4 px-4">10</td>
-          </tr>
-          <tr>
-            <td class="border py-4 px-4">a</td>
-            <td class="border py-4 px-4">Harina</td>
-            <td class="border py-4 px-4">Produkt</td>
-            <td class="border py-4 px-4">10223</td>
-            <td class="border py-4 px-4">Hanna son praptar</td>
-            <td class="border py-4 px-4">10 km</td>
-            <td class="border py-4 px-4">22:08 2024</td>
-            <td class="border py-4 px-4">10</td>
-          </tr>
-          <tr>
-            <td class="border py-4 px-4">a</td>
-            <td class="border py-4 px-4">Harina</td>
-            <td class="border py-4 px-4">Produkt</td>
-            <td class="border py-4 px-4">11223</td>
-            <td class="border py-4 px-4"></td>
-            <td class="border py-4 px-4"></td>
-            <td class="border py-4 px-4">23-09-2004</td>
-            <td class="border py-4 px-4">10</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="col-start-4 col-end-8 flex justify-center mt-7">
+        <h1 class="text-3xl font-bold">Productos</h1>
+      </div>
+      <div class="col-start-8 col-end-10 row-start-3 row-end-4 flex justify-evenly">
+        <a href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+
+        </a>
+        <a href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+        </a>
+        <a href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
+        </a>
+      </div>
+        <table class="w-full col-start-2 col-end-10 row-start-4 row-end-8 rounded-xl border-separate border-spacing-0 overflow-hidden border border-black">
+  <thead>
+    <tr>
+      <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left border border-black rounded-tl-xl">Lote</th>
+      <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left border border-black">Nombre</th>
+      <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left border border-black">Presentacion</th>
+      <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left border border-black">Fecha de Vencimiento</th>
+      <th class="bg-gray-200 text-gray-600 py-3 px-4 text-left border border-black rounded-tr-xl">Stock</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border py-4 px-4 border-black">10023</td>
+      <td class="border py-4 px-4 border-black">Producto A</td>
+      <td class="border py-4 px-4 border-black">10 kg</td>
+      <td class="border py-4 px-4 border-black">05-04-2024</td>
+      <td class="border py-4 px-4 border-black">15</td>
+    </tr>
+    <tr>
+      <td class="border py-4 px-4 border-black">10223</td>
+      <td class="border py-4 px-4 border-black">Producto B</td>
+      <td class="border py-4 px-4 border-black">5 kg</td>
+      <td class="border py-4 px-4 border-black">22-08-2024</td>
+      <td class="border py-4 px-4 border-black">25</td>
+    </tr>
+    <tr>
+      <td class="border py-4 px-4 border-black">11223</td>
+      <td class="border py-4 px-4 border-black">Producto C</td>
+      <td class="border py-4 px-4 border-black">1 kg</td>
+      <td class="border py-4 px-4 border-black">23-09-2024</td>
+      <td class="border py-4 px-4 border-black">30</td>
+    </tr>
+  </tbody>
+</table>
+
     </div>
 @endsection
